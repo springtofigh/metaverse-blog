f358d9d9708c1d0ad0abe27290ce9ddd15d79c32
BT <bahartofigh@gmail.com>
2023-09-25 22:11:19 +0330

    finnish Card Explore and Explore

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -5,10 +5,43 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
-const ExploreCard = () => (
-  <div>
-    ExploreCard
-  </div>
-);
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
+  return (
+    <motion.div
+    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? "flex-[10] lg:flex-[3.5]" : 
+      "flex items-center justify-center flex-[2] min-w-[170px] h-[700px] transition-[flex] duration-[0.75] ease-out-flex cursor-pointer lg:flex-[0.5]"
+    }`}
+    onClick={() => handleClick(id)}
+    >
+      <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full rounded-[24px] object-cover"
+      />
+      {
+        active !== id ? (
+          <h3 className="font-semibold text-[18px] text-white absolute z-0 sm:text-[26px] lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">{title}</h3>
+        ) : (
+          <div className='absolute bottom-0 p-8 w-full flex flex-col justify-start'>
+            <div className={`${styles.flexCenter} mb-[16px] w-[60px] h-[60px] rounded-[24px] glassmorphism`}>
+              <img 
+              src="public/images/headset.svg" 
+              alt="لوگو"
+              className="w-1/2 h-1/2 object-contain"/>
+            </div>
+            <p className="font-normal text-[16px] leading-[20.16px] text-white">
+              وارد متاورس شو
+            </p>
+            <h2 className="mt-[24px] font-semibold text-white text-[24px] sm:text-[32px]">
+            {title}
+            </h2>?
+          </div>
+        )
+      }
+    </motion.div>
+  )
+}
 
 export default ExploreCard;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -10,6 +10,7 @@ import styles from '../styles';
 
 
 const Explore = () => {
+  const [active , setActive] = useState("planet-02")
   return (
     <div className={`${styles.paddings}`} id="explore">
       <motion.div
@@ -23,6 +24,19 @@ const Explore = () => {
         <TitleText
           title={<>انتخاب دنیایی که میخواهی در آن <br className="hidden md:block" /> کاوش کنی</>}
           textStyles="text-center"/>
+          <div className="flex flex-col mt-[50px] gap-5 min-h-[70vh] lg:flex-row">
+            {
+              exploreWorlds.map((world, index) => (
+                <ExploreCard
+                key={world.id}
+                {...world}
+                index={index}
+                active={active}
+                handleClick={setActive}
+                />
+              ))
+            }
+          </div>
       </motion.div>
     </div>
   )
