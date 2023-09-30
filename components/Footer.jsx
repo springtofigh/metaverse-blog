4ff1cb075eba6b7cf5fbc45b070c2c28d2ae6b4b
BT <bahartofigh@gmail.com>
2023-09-30 17:17:38 +0330

    debug Whats New component and finish Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,9 +7,56 @@ import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
 const Footer = () => (
-  <footer>
-    Footer
-  </footer>
+  <motion.footer
+  variants={footerVariants}
+  initial="hidden"
+  whileInView="show"
+  className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='footer-gradient'/>
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className='flex items-center justify-between gap-6'>
+        <button type='submit' className='flex items-center justify-between px-6 py-4 gap-3 bg-[#25618B] rounded-[32px]'>
+        <span className='font-normal text-[16px] text-white'>
+          وارد متاورس بشو
+        </span>
+          <img
+          src='/images/headset.svg'
+          alt='headset'
+          className='w-[24px] h-[24px] object-contain'
+          />
+        </button>
+        <h4 className='font-bold text-[44px] text-white md:text-[64px]'>
+        وارد متاورس بشو
+        </h4>
+      </div>
+
+      <div className='mb-[50px] mt-[70px] h-[2px] bg-white opacity-10'/>
+
+      <div className='flex flex-col'>
+        <div className='flex items-center justify-between flex-wrap'>
+          <div className='flex gap-7'>
+            {
+              socials.map((social) => (
+                <img 
+                key={social.name}
+                src={social.url} 
+                alt={social.name}
+                className='w-[24px] h-[24px] object-contain cursor-pointer'
+                />
+              ))
+            }
+          </div>
+        <p className='font-normal text-[14px] text-white opacity-70'>
+        کلیه حقوق زیر نظر انجمن بلاکچین ایران است.Copyright © 2021 - 2022
+        </p>
+        <h4 className='font-extrabold text-[24px] text-white'>
+        متاورس فارسی
+        </h4>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -9,45 +9,45 @@ import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
 
 
 const WhatsNew = () => {
-  return (
-    <sction className={`${styles.paddings} relative z-10`}>
-      <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{once: "false", amount: 0.25}}
-      className={`${styles.innerWidth} mx-auto flex flex-col gap-8 lg:flex-row`}
-      >
+    return (
+        <section className={`${styles.paddings} relative z-10`}>
         <motion.div
-        variants={planetVariants("right")}
-        className={`${styles.flexCenter} flex-1`}
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once:"false", amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-8`}
         >
-        <img
-          src="/images/whats-new.png"
-          alt="اخبار جدید"
-          className="w-[90%] h-[90%] object-contain"
-        />
-        </motion.div>
-        <motion.div
-        variants={fadeIn("right", "tween", 0.2 ,1)}
-        className="flex flex-col justify-center flex-[0.95]"
-        >
+            <motion.div
+            variants={planetVariants("left")}
+            className={`${styles.flexCenter} flex-1`}
+            >
+            <img
+            src="/images/whats-new.png"
+            alt="شروع کن"
+            className="w-[90%] h-[90%] object-contain"
+            />
+            </motion.div>
+            <motion.div
+            variants={fadeIn("left", "tween", 0.2, 1)}
+            className="flex flex-col flex-[0.75] justify-center"
+            >
         <TypingText title="| تازه ترین ها چیست؟" />
         <TitleText title={<>چه چیزی درباره متاورس تازه است؟</>} />
-        <div className="flex justify-start items-center flex-wrap mt-[48px] gap-[24px]">
-          {
-            newFeatures.map((feature) => (
-              <NewFeatures
-              key={feature.title}
-              {...feature}
-              />
-            ))
-          }
-        </div>
+            <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+            {
+                newFeatures.map((feature) => (
+                    <NewFeatures
+                    key={feature.title}
+                    {...feature}
+                />
+                ))
+            }
+            </div>
+            </motion.div>
         </motion.div>
-      </motion.div>
-    </sction>
-  )
+        </section>
+    )
 }
 
 export default WhatsNew;
