d5bed9cbed41b0b0fc64ce7f173d4056e9f46777
BT <bahartofigh@gmail.com>
2023-10-01 18:56:57 +0330

    work on World component and add new images for this component.

--- sections/World.jsx
+++ sections/World.jsx
@@ -3,10 +3,48 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+import { TitleText, TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 const World = () => {
   return (
-    <div>World</div>
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      >
+        <TypingText title="| مردم در همه جای دنیا" textStyles="text-center" />
+        <TitleText 
+        title={(<>دوستان اطراف خود را دنبال کنید و از آنها دعوت کنید تا در یک دنیا با هم بازی کنند</>)}
+        textStyles="text-center"
+        />
+      </motion.div>
+
+      <motion.div
+      variants={fadeIn("up" , "tween", 0.3, 1)}
+      initial="hidden"
+      whileInView="show"
+      className='relative mt-[70px] w-full h-[526px] flex'
+      >
+        <img
+        src='/images/map.png'
+        alt=''
+        className='w-full h-full object-contain'
+        />
+
+        <div className='absolute bottom-20 right-20 p-[6px] w-[70px] h-[70px] bg-slate-500 rounded-full'>
+          <img src='/images/people-01.png' alt='people-1' className='w-full h-full' />
+        </div>
+        <div className='absolute top-5 left-20 p-[6px] w-[70px] h-[70px] bg-slate-500 rounded-full'>
+          <img src='/images/people-02.png' alt='people-2' className='w-full h-full' />
+        </div>
+        <div className='absolute top-1/2 left-[45%] p-[6px] w-[70px] h-[70px] bg-slate-500 rounded-full'>
+          <img src='/images/people-03.png' alt='people-3' className='w-full h-full' />
+        </div>
+      </motion.div>
+    </section>
   )
 }
 
