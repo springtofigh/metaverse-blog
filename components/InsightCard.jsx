e439a086850ac4f89be95a2dd21b07e23f054e80
BT <bahartofigh@gmail.com>
2023-10-03 18:04:08 +0330

    Insight component is done

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -4,10 +4,32 @@ import { motion } from 'framer-motion';
 
 import { fadeIn } from '../utils/motion';
 
-const InsightCard = () => (
-  <div>
-    InsightCard 
-  </div>
+const InsightCard = ({ title, subtitle, imgUrl, index }) => (
+  <motion.div
+  variants={fadeIn("up", "spring", index * 0.5, 1)}
+  className='flex flex-col gap-6 md:flex-row'
+  >
+    <img 
+    src={imgUrl} 
+    alt=''  
+    className='w-full h-[250px] object-cover rounded-[32px] md:w-[270px]'/>
+
+    <div className='flex items-center justify-between w-full'>
+      <div className='flex-1 max-w-[] flex-col md:mr-[62px]'>
+      <h4 className='font-normal text-[26px] text-white lg:text-[42px]'>{title}</h4>
+      <p className='font-normal text-[14px] text-secondary-white lg:text-[20px]'>{subtitle}</p>
+      </div>
+
+    <div 
+    className='hidden w-[100px] h-[100px] items-center justify-center bg-white bg-transparent rounded-full border-[2px] lg:flex'>
+      <img 
+      src='/images/arrow.svg' 
+      alt='arrow' 
+      className='w-[48px] h-[48px] object-contain'
+      />
+    </div>
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -3,11 +3,36 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+import { insights } from '../constants';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { staggerContainer } from '../utils/motion';
 
 
 const Insights = () => {
   return (
-    <div>Insights</div>
+    <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| بینش متاورس" textStyles="text-center" />
+        <TitleText  title={<>بینش در مورد متاورس</>} textStyles="text-center" />
+        <div className='mt-[50px] flex flex-col gap-8'>
+          {
+            insights.map((insight, index)=> (
+              <InsightCard
+              key={`insight-${index}`}
+              index={index + 1}
+              {...insight}
+              />
+            ))
+          }
+        </div>
+      </motion.div>
+    </section>
   )
 }
 
