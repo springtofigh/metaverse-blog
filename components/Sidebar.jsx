e337ccfb058527d8c19b07a03c64fb588bc46583
BT <bahartofigh@gmail.com>
2023-10-15 17:47:23 +0330

    add x-mark SVG and develope Sidebar component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,73 +1,91 @@
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Sidebar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
   return (
-        <div
-        class="sidebar fixed top-0 bottom-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900 z-20 lg:right-0"
-        >
-            <div class="text-gray-100 text-xl">
-            <div class="p-2.5 mt-1 flex items-center">
-                <h1 class="font-extrabold text-gray-200 text-[24px] ml-3">
-                متاورس فارسی
-                </h1>
-            </div>
-            <div class="my-2 bg-gray-600 h-[1px]"></div>
-            </div>
+    <>
+        {
+            isSidebarOpen 
+            && 
             <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+            class="fixed top-0 bottom-0 w-[300px] right-0 p-2 text-center bg-gray-900 overflow-y-auto z-20"
             >
-            <Link href="/">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-                خانه
-            </span>
-            </Link>
+                    <div class="text-gray-100 text-xl">
+                    <div class="p-2.5 mt-1 flex items-center justify-between">
+                        <h1 class="font-extrabold text-gray-200 text-[24px] ml-3">
+                        متاورس فارسی
+                        </h1>
+                        <button onClick={toggleSidebar}>
+                            <img className="w-[24px] h-[24px] bg-white object-contain" src="/images/x-mark.svg" alt="x-mark"/>
+                        </button>
+                    </div>
+                    <div class="h-[1px] bg-gray-600 my-2"></div>
+                    </div>
+                    <div
+                    class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                    >
+                    <Link href="#home" onClick={toggleSidebar}>
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                        خانه
+                    </span>
+                    </Link>
+                    </div>
+                <div
+                class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                >
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                    <Link href="#about" onClick={toggleSidebar}>
+                        درباره متاورس
+                    </Link>
+                    </span>
+                </div>
+                <div
+                class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                >
+                    <Link href="#explore" onClick={toggleSidebar}>
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                        جهان ها
+                    </span>    
+                    </Link>
+                </div>
+                <div
+                class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                >
+                    <Link href="#know-metavers" onClick={toggleSidebar}>
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                        آشنایی با متاورس
+                    </span>
+                    </Link>
+                </div>
+                <div
+                class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                >
+                    <Link href="#newest" onClick={toggleSidebar}>
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                        تازه ترین ها
+                    </span>
+                    </Link>
+                </div>
+                <div
+                class="flex items-center p-2.5 mt-3 px-4 rounded-md duration-300 cursor-pointer text-white hover:bg-blue-600"
+                >
+                    <Link href="#articles" onClick={toggleSidebar}>
+                    <span class="font-bold text-[15px] ml-4 text-gray-200">
+                        مقالات
+                    </span>
+                    </Link>
+                </div>
             </div>
-            <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-            >
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-            <Link href="/about">
-                درباره متاورس
-            </Link>
-            </span>
-            </div>
-            <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-            >
-            <Link href="/worlds">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-                جهان ها
-            </span>    
-            </Link>
-            </div>
-            <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-            >
-            <Link href="/know-metavers">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-                آشنایی با متاورس
-            </span>
-            </Link>
-            </div>
-            <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-            >
-            <Link href="/newest">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-                تازه ترین ها
-            </span>
-            </Link>
-            </div>
-            <div
-            class="flex items-center p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-            >
-            <Link href="/articles">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">
-                مقالات
-            </span>
-            </Link>
-            </div>
-        </div>
+        }
+    </>
   )
 }
 
