eecc1dcb18d0baf62e452a8ae0fd9f496d828080
BT <bahartofigh@gmail.com>
2023-10-15 21:43:52 +0330

    add relative position to button to solve Sidebar showing problem

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     >
       <div className="absolute w-[50%] inset-0 gradient-01"/>
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <button onClick={handlemenuClick}>
+      <button className="relative" onClick={handlemenuClick}>
         <img className="w-[24px] h-[24px] object-contain" src="/images/menu.svg" alt="menu"/>
       </button>
         <h2 className="font-extrabold text-[24px] text-white leading-[30px]">
@@ -32,7 +32,7 @@ const Navbar = () => {
         <img className="w-[24px] h-[24px] object-contain" src="/images/search.svg" alt="search"/>
       </div>
     </motion.nav> 
-    { showSidebar && <Sidebar/> }
+    { showSidebar && <Sidebar /> }
 </>
   )
 }
