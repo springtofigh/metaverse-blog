a3b206205826133c9a6c824d7698e02bce41986c
BT <bahartofigh@gmail.com>
2023-10-21 21:24:21 +0330

    add text and images on map posters and make location pin for images

--- sections/World.jsx
+++ sections/World.jsx
@@ -34,20 +34,49 @@ const World = () => {
         className='w-full h-full object-contain'
         />
 
-        <div className='absolute bottom-40 right-5 p-[4px] w-[40px] h-[40px] bg-slate-500 rounded-full lg:bottom-20 lg:right-20 md:w-[70px] md:h-[70px] sm:p-[6px] sm:bottom-[190px] sm:right-10'>
+        <div className='absolute bottom-40 right-5 p-[4px] w-[40px] h-[40px] bg-zinc-600 rounded-full lg:bottom-20 lg:right-24 md:w-[70px] md:h-[70px] sm:p-[6px] sm:bottom-[190px] sm:right-10'>
           <img src='/images/people-01.png' alt='people-1' className='w-full h-full' />
+          <div className='pin bg-zinc-600' />
         </div>
-        <div className='absolute top-40 right-8 w-[70px] h-[60px] p-[6px] bg-slate-500 rounded-[24px] sm:top-30 sm:right-24 md:w-[140px] md:h-[116px] lg:w-[180px] lg:h-[136px] md:top-[120px] md:right-10 lg:top-[80px] lg:right-40'>
+        <div className='absolute top-40 right-8 w-[70px] h-[60px] p-[6px] bg-zinc-600 rounded-[24px] sm:top-30 sm:right-24 md:w-[140px] md:h-[116px] lg:w-[180px] lg:h-[136px] md:top-[120px] md:right-10 lg:top-[80px] lg:right-40'>
           <img src='/images/map-poster2.png' alt='map-poster-2' className='w-full h-full' />
+          <div className='hidden mt-[-60px] px-3 md:block'>
+          <div className='flex items-center justify-between'>
+            <img src='/images/people-06.png' alt="people-6"/>
+            <img src='/images/people-05.png' alt="people-5" className='-mr-4' />
+            <img src='/images/people-04.png' alt="people-4" className='-mr-4' />
+            <p className='font-normal text-white text-[10px] lg:text-[12px]'>
+            + ۲۶۴ اضافه شدند
+            </p>
+          </div>
+            <p className='font-bold text-white text-[14px] leading-5 lg:text-[18px]'>
+              آزمایشگاه هاوکینز
+            </p>
+          </div>
         </div>
-        <div className='absolute top-[35%] left-2 p-[4px] w-[40px] h-[40px] bg-slate-500 rounded-full lg:top-[70px] lg:left-[90px] md:w-[70px] md:h-[70px] sm:p-[6px] sm:top-[160px] sm:left-8'>
+        <div className='absolute top-[35%] left-2 p-[4px] w-[40px] h-[40px] bg-zinc-600 rounded-full lg:top-[70px] lg:left-[90px] md:w-[70px] md:h-[70px] sm:p-[6px] sm:top-[160px] sm:left-8'>
           <img src='/images/people-02.png' alt='people-2' className='w-full h-full' />
+          <div className='pin bg-zinc-600' />
         </div>
-        <div className='absolute top-[45%] left-[15%] w-[70px] h-[60px] p-[6px] bg-slate-500 rounded-[24px] sm:top-[35%] sm:left-[20%] md:w-[140px] md:h-[116px] lg:w-[180px] lg:h-[136px]'>
+        <div className='absolute top-[45%] left-[15%] w-[70px] h-[60px] p-[6px] bg-zinc-600 rounded-[24px] sm:top-[45%] sm:left-[20%] md:w-[140px] md:h-[116px] lg:w-[180px] lg:h-[136px]'>
           <img src='/images/map-poster1.png' alt='map-poster-1' className='w-full h-full' />
+          <div className='hidden mt-[-60px] px-3 md:block'>
+          <div className='flex items-center justify-between'>
+            <img src='/images/people-06.png' alt="people-6"/>
+            <img src='/images/people-05.png' alt="people-5" className='-mr-4' />
+            <img src='/images/people-04.png' alt="people-4" className='-mr-4' />
+            <p className='font-normal text-white text-[10px] lg:text-[12px]'>
+            + ۲۶۴ اضافه شدند
+            </p>
+          </div>
+            <p className='font-bold text-white text-[14px] leading-5 lg:text-[18px]'>
+              جهان وارونه
+            </p>
+          </div>
         </div>
-        <div className='absolute top-[45%] left-[45%] p-[4px] w-[40px] h-[40px] bg-slate-500 rounded-full lg:top-1/2 lg:left-[48%] md:w-[70px] md:h-[70px] sm:p-[6px]'>
+        <div className='absolute top-[45%] left-[45%] p-[4px] w-[40px] h-[40px] bg-zinc-600 rounded-full lg:top-1/2 lg:left-[48%] md:w-[70px] md:h-[70px] sm:p-[6px]'>
           <img src='/images/people-03.png' alt='people-3' className='w-full h-full' />
+          <div className='pin relative w-[40px] h-[40px] -mt-[65px] bg-zinc-600 md:w-[70px] md:h-[70px]' />
         </div>
       </motion.div>
     </motion.div>
