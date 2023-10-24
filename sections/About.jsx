7a3e1f1ea716d1a7d4f9cbc3016a5b347003a45a
BT <bahartofigh@gmail.com>
2023-10-24 23:37:13 +0330

    add background gradient

--- sections/About.jsx
+++ sections/About.jsx
@@ -9,6 +9,7 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 const About = () => {
   return (
     <section className={`${styles.paddings} relative z-10`} id="about">
+      <div className="gradient-02 z-0" />
       <motion.div
       variants={staggerContainer}
       initial="hidden"
